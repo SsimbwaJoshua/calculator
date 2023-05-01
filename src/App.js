59c677d7044df28134191b64580dcd9799ebaf77
SsimbwaJoshua <ssimbwajoshua124@gmail.com>
2023-05-01 14:58:14 +0300

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className="tittle">
+          <h1>SPLI</h1>
+          <h1>TTER</h1>
+        </div>
+        <div className="whole-calc">
+          <div>
+            <h2>Bill</h2>
+            <form>
+              <input placeholder="$" className="input-field" />
+            </form>
+            <h2>select Tip %</h2>
+            <div>
+              <button>5%</button>
+              <button>10%</button>
+              <button>15%</button>
+              <button>25%</button>
+              <button>50%</button>
+              <button>Custom</button>
+            </div>
+            <form>
+              <input placeholder="##" className="input-field" />
+            </form>
+          </div>
+          <div>
+            <div>
+              <div>
+                <h2>Tip Amount</h2>
+                <p>/ person</p>
+              </div>
+              <p>$ 0.00</p>
+            </div>
+            <div>
+              <div>
+                <h2>Total</h2>
+                <p>/ person</p>
+              </div>
+              <p>$ 0.00</p>
+            </div>
+            <button>RESET</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
